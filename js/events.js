768a79287a2dd2fb799a541b1eb28f4bb4c212cf
Keith Jeter <kjeter2001@yahoo.com>
2018-01-18 04:59:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,37 @@
 
 $(document).ready(function(){
 
+function getIt() {
+  $('p').on("click", function () {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('#typing').on("keydown", function(press) {
+    if (press.which === 71) {
+      alert("You have entered g.");
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+
+$(document).ready(function(){
+
 // call functions here
+
+});
 
 });
